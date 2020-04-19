f57456f81144f6f3a5edc79db5cd5c1f104d04ae
Adarsha <adarshacr@outlook.com>
2020-04-19 22:22:50 +0530

    Added custom exception class

--- src/main/java/com/acr/spring_security_jwt/Manager/UserManager.java
+++ src/main/java/com/acr/spring_security_jwt/Manager/UserManager.java
@@ -2,6 +2,7 @@ package com.acr.spring_security_jwt.Manager;
 
 import com.acr.spring_security_jwt.DAL.IUserDAL;
 import com.acr.spring_security_jwt.Entity.User;
+import com.acr.spring_security_jwt.Utility.NoResultException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -12,9 +13,12 @@ public class UserManager {
     @Autowired
     private IUserDAL userDAL;
 
-    public List<User> getUsers()
-    {
+    public List<User> getUsers() throws NoResultException {
         List<User> users=userDAL.findAll();
+        if(users==null || users.size()==0)
+        {
+            throw new NoResultException("No User exists");
+        }
         return userDAL.findAll();
     }
 }
